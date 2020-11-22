7d6eba62dc01ee565d337b50148c7779214eb74c
Yana <yatskiv3854@ukr.net>
2020-11-22 15:56:41 +0200

    task added

---
+++ Task.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+
+finally:
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
